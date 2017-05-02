421c59663a2d866742ce7b2bfaf339dedd69ac12
BenceZambo <bence.zambo96@gmail.com>
2017-05-02 14:01:05 +0200

    New question html file is nearly done

--- new_question.py
+++ new_question.py
@@ -12,10 +12,15 @@ def new_question():
     Add the new question to question.csv,
     and return to the home page.
     """
-    stories = openfile()
+    datas = openfile()
     new_list = []
-    new_list.append(0, ID_generator(stories))
-    return
+    new_list.append(0, ID_generator(datas))
+    question_attributes = ['id', 'submisson_time', 'title', 'message']
+    for element in question_attributes:
+        new_list.append(element)
+    datas.append(new_list)
+    write_to_file(datas)
+    return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
