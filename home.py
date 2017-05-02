772f68bbaa59c8627536c4bd4d45e744962ffcea
KrisztianKardos <frekazoid85@gmail.com>
2017-05-02 14:03:41 +0200

    home.py + home.html updated

--- home.py
+++ home.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 import csv
 from common import *
 
+'''
+app = Flask(__name__)
+'''
+
 
 @app.route("/", methods=["POST", "GET"])
 def home_list_handler():
@@ -14,3 +18,8 @@ def home_list_handler():
                     "Delete"
                     ]
     return render_template("home.html", questions=questions, table_headers=table_headers)
+
+'''
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
