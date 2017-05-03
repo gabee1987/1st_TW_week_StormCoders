33458dd00e0c832e9d94385bee93eed9b5c86798
gabee1987 <gabee1987@gmail.com>
2017-05-03 11:44:13 +0200

    refactor the id generator and datamanager modules

--- common.py
+++ common.py
@@ -9,25 +9,14 @@ import random
 import constants
 
 
-def ID_generator(table):
+def ID_generator():
     """
         Generates an ID for the questions and answers.
         The ID is unique in the database.
     """
-    generated = table[0][0]
-    char_list = [
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "abcdefghijklmnopqrstuvwxyz",
-        "0123456789",
-        "!@#$%^&*()?=<>[]|+-/"
-    ]
-    while generated in [x[0] for x in table]:
-        generated = ""
-        for char in char_list:
-            generated += "".join([random.choice(char) for _ in range(2)])
-    generated = list(generated)
-    random.shuffle(generated)
-    return "".join(generated)
+    generated_q_ids = []
+    generated_a_ids = []
+
 
 
 def open_question_file(filepath):
@@ -39,7 +28,7 @@ def open_question_file(filepath):
     decode = ENCODE_QUESTION_FIELDS
 
     with open(filepath, 'r') as workfile:
-        row = workfile.readlines()
+        reader = csv.DictReader(filepath, fieldnames=QUESTION_FIELDS, delimiter(','))
         row["0"] = int(row["0"])
         for field in decode:
             row[4][5] = base64.b64decode(row[field]).decode()
@@ -58,7 +47,7 @@ def open_answer_file(filepath):
     with open(filepath, 'r') as workfile:
         row = workfile.readlines()
         row["0"] = int(row["0"])
-        row[4][5] = base64.b64decode(row[field]).decode()
+        row[4:5] = base64.b64decode(row[field]).decode()
         table = [item.strip("\n").split(';') for item in row]
         return table
 
